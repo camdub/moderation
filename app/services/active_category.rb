09a6d6be6fea4b020223c906a585be50f451d25f
Cameron Woodmansee <cameron29w@gmail.com>
2013-10-17 18:44:48 -0400

    Updated active category query to use active flag on weeks table

--- app/services/active_category.rb
+++ app/services/active_category.rb
@@ -16,11 +16,8 @@ class ActiveCategory
     end
 
     def get_record(limit_level)
-        CategoryHistory.
-          includes(:limit_level, :week).
-          where(limit_levels: { name: limit_level }).
-          references(:limit_level).order("weeks.start_date DESC")
-          .first
+      CategoryHistory.joins(:week, :limit_level)
+      .where(weeks: {active: true}, limit_levels: {name: limit_level}).first
     end
   end
 
